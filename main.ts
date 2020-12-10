cc3d47250773ae6f48113b97040decdc936bf3cc
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2020-12-10 13:13:40 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@
 //         light.clear()
 while (true) {
     console.log("Light Level:" + input.lightLevel())
-    if (input.lightLevel() < 7) {
+    if (input.lightLevel() < 4) {
         light.setAll(light.rgb(0, 0, 255))
     } else if (input.lightLevel() > 0) {
         light.setAll(light.rgb(255, 255, 0))
